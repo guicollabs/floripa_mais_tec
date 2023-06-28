f6aac4feb36335720763c188e99fae313fa08458
Guilherme Ferreira <guilherme.tecnologiaweb@gmail.com>
2023-06-28 20:59:48 -0300

    Finalizando Exercicio 2 - Semana 6

---
+++ Modulo_2/Semana_6/Exercicio_02/src/App.jsx
@@ -0,0 +1,14 @@
+import { Header } from './components/Header';
+import { Banner } from './components/Banner';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Banner />
+    </>
+  );
+}
+
+export default App;
