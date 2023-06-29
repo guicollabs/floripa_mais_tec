edd543ad41d664b024589b94a2c8380153a0c3fa
Guilherme Ferreira <guilherme.tecnologiaweb@gmail.com>
2023-06-29 19:36:48 -0300

    Finalizando Exercício 3 - Semana 6

---
+++ Modulo_2/Semana_6/Exercicio_03/src/components/Produtos/Produtos.jsx
@@ -0,0 +1,17 @@
+import styles from './Produtos.module.css';
+
+const Produtos = () => {
+  return (
+    <section className={styles.produtos}>
+      <div>
+        <h2>Nossos Cupcakes</h2>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis soluta
+          numquam cum animi quos itaque quisquam nulla nemo quidem voluptatem?
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Produtos;
